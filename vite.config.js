948285a20c382fb7dc2d430294f32572b4f076fa
StefanoDaniello <stefanodaniello68@gmail.com>
2024-10-21 18:58:52 +0200

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        // host: "0.0.0.0", // Per permettere a Vite di essere accessibile da altre macchine
+        port: 5174, // porta scelta per il backend
+    },
 });
